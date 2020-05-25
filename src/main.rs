5b6d41fbc34197fa8fea65826146d66c3507d96a
lorenmh <neutronstar@gmail.com>
2020-05-24 22:16:58 -0400

    box

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 extern crate sdl2;
 
-use sdl2::pixels::Color;
 use sdl2::event::Event;
+use sdl2::pixels;
 use sdl2::keyboard::Keycode;
-use std::time::Duration;
 
-pub fn main() -> Result<(), String> {
+use sdl2::gfx::primitives::DrawRenderer;
+
+const SCREEN_WIDTH: u32 = 800;
+const SCREEN_HEIGHT: u32 = 600;
+
+fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
-    let video_subsystem = sdl_context.video()?;
-
-    let window = video_subsystem.window("rust-sdl2 demo: Video", 800, 600)
+    let video_subsys = sdl_context.video()?;
+    let window = video_subsys.window("rust-sdl2_gfx: draw line & FPSManager", SCREEN_WIDTH, SCREEN_HEIGHT)
         .position_centered()
         .opengl()
         .build()
@@ -17,25 +20,38 @@ pub fn main() -> Result<(), String> {
 
     let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
 
-    canvas.set_draw_color(Color::RGB(255, 0, 0));
+    canvas.set_draw_color(pixels::Color::RGB(0, 0, 0));
     canvas.clear();
     canvas.present();
-    let mut event_pump = sdl_context.event_pump()?;
 
-    'running: loop {
-        for event in event_pump.poll_iter() {
+    let mut lastx = 0;
+    let mut lasty = 0;
+
+    let mut events = sdl_context.event_pump()?;
+
+    let color = pixels::Color::RGB(255, 100, 100);
+    let _ = canvas.line(50, 50, 750, 50, color);
+    let _ = canvas.line(750, 50, 750, 550, color);
+    let _ = canvas.line(750, 550, 50, 550, color);
+    let _ = canvas.line(50, 550, 50, 50, color);
+    canvas.present();
+
+    'main: loop {
+        for event in events.poll_iter() {
+
             match event {
-                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
+
+                Event::Quit {..} => break 'main,
+
+                Event::KeyDown {keycode: Some(keycode), ..} => {
+                    if keycode == Keycode::Escape {
+                        break 'main
+                    }
+                }
+
                 _ => {}
             }
         }
-
-        canvas.clear();
-        canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
-        // The rest of the game loop goes here...
     }
 
     Ok(())
