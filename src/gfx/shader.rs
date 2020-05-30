9da04d7cfff839b74d78d36c598f09542abf6abf
lorenmh <neutronstar@gmail.com>
2020-05-30 00:07:18 -0400

    hello triangle

--- src/gfx/shader.rs
+++ src/gfx/shader.rs
@@ -4,15 +4,41 @@ use gl::types::{GLchar, GLint, GLuint};
 
 fn attach_program() {}
 
-type shader_program = GLuint;
+type ShaderProgram = GLuint;
+type Program = GLuint;
 
 #[repr(u32)]
-enum Type {
+pub enum Type {
     Vertex = gl::VERTEX_SHADER,
     Fragment = gl::FRAGMENT_SHADER,
 }
 
-fn compile_shader(glsl: &str, typ: Type) -> Result<shader_program, std::string::String> {
+pub fn link_program(vs: ShaderProgram, fs: ShaderProgram) -> Result<Program, std::string::String> {
+    unsafe {
+        let program = gl::CreateProgram();
+        gl::AttachShader(program, vs);
+        gl::AttachShader(program, fs);
+        gl::LinkProgram(program);
+        // Get the link status
+        let mut status = gl::FALSE as GLint;
+        gl::GetProgramiv(program, gl::LINK_STATUS, &mut status);
+
+        // Fail on error
+        if status != (gl::TRUE as GLint) {
+            let mut len: GLint = 0;
+            gl::GetProgramiv(program, gl::INFO_LOG_LENGTH, &mut len);
+            let mut buf = Vec::new();
+            buf.set_len((len as usize) - 1); // subtract 1 to skip the trailing null character
+            gl::GetProgramInfoLog(program, len, std::ptr::null_mut(), buf.as_mut_ptr() as *mut GLchar);
+            let s = std::str::from_utf8(buf.as_slice()).ok().expect("ProgramInfoLog not valid utf8");
+            return Err(s.to_string())
+        }
+        Ok(program as Program)
+    }
+}
+
+
+pub fn compile_shader(glsl: &str, typ: Type) -> Result<ShaderProgram, std::string::String> {
     let program;
 
     unsafe {
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use sdl2::keyboard::Keycode;
 use sdl2::pixels;
 use rand::Rng;
 use sdl2::video::GLProfile;
+use std::ffi::CString;
+use gl;
+use gl::types::{GLfloat, GLsizeiptr, GLuint, GLboolean};
 
 mod gfx;
 
@@ -23,6 +26,26 @@ struct Rect {
     color: pixels::Color,
     vec: [f32; 2],
 }
+
+static VERTEX_DATA: [GLfloat; 6] = [
+    0.0,  0.5,
+    0.5, -0.5,
+    -0.5, -0.5
+];
+
+static VS_SRC: &'static str =
+    "#version 150\n\
+    in vec2 position;\n\
+    void main() {\n\
+    gl_Position = vec4(position, 0.0, 1.0);\n\
+    }";
+
+static FS_SRC: &'static str =
+    "#version 150\n\
+    out vec4 out_color;\n\
+    void main() {\n\
+       out_color = vec4(1.0, 1.0, 1.0, 1.0);\n\
+    }";
 
 fn handle_events(events: &mut sdl2::EventPump) -> Action {
     for event in events.poll_iter() {
@@ -74,9 +97,43 @@ fn main() -> Result<(), String> {
 
     let mut objects: Vec<gfx::object::Object> = Vec::new();
 
+    let vs = gfx::shader::compile_shader(VS_SRC, gfx::shader::Type::Vertex)?;
+    let fs = gfx::shader::compile_shader(FS_SRC, gfx::shader::Type::Fragment)?;
+
+    let program = gfx::shader::link_program(vs, fs)?;
+
+    let mut vao = 0;
+    let mut vbo = 0;
+
+    unsafe {
+        // Create Vertex Array Object
+        gl::GenVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
+
+        // Create a Vertex Buffer Object and copy the vertex data to it
+        gl::GenBuffers(1, &mut vbo);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(gl::ARRAY_BUFFER,
+                       (VERTEX_DATA.len() * std::mem::size_of::<GLfloat>()) as GLsizeiptr,
+                       std::mem::transmute(&VERTEX_DATA[0]),
+                       gl::STATIC_DRAW);
+
+        // Use shader program
+        gl::UseProgram(program);
+        let id1 = CString::new("out_color").expect("foop");
+        gl::BindFragDataLocation(program, 0, id1.as_ptr());
+
+        // Specify the layout of the vertex data
+        let id2 = CString::new("position").expect("boop");
+        let pos_attr = gl::GetAttribLocation(program, id2.as_ptr());
+
+        gl::EnableVertexAttribArray(pos_attr as GLuint);
+        gl::VertexAttribPointer(pos_attr as GLuint, 2, gl::FLOAT, gl::FALSE as GLboolean, 0, std::ptr::null());
+    }
+
     'main: loop {
         unsafe {
-            gl::ClearColor(1.0, 0.0, 0.0, 1.0);
+            gl::ClearColor(0.2, 0.2, 0.2, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
         }
 
@@ -90,6 +147,10 @@ fn main() -> Result<(), String> {
         let now = timer.ticks();
         let delta = now - tick;
         tick = now;
+
+        unsafe {
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+        }
 
         // scene
         //render_scene(&mut canvas, &mut objects, delta);
