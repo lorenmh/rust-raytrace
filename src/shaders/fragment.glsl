224fe654ec63f56b1e9cc233515fbcde2a84b275
lorenmh <neutronstar@gmail.com>
2020-06-13 01:27:09 -0400

    ..

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -3,9 +3,20 @@
 out vec4 FragColor;
 
 in vec3 color;
+in vec4 gl_FragCoord;
 
 uniform float clock;
 
 void main() {
-   FragColor = vec4(color.x * clock, color.y * clock, color.z * clock, 1.0);
+   float s = mod(floor(gl_FragCoord.x / 5.0), 2.0) * 0.02;
+   float t = mod(floor(gl_FragCoord.y / 5.0), 2.0) * 0.02;
+   //float t = mod(floor(gl_FragCoord.y / 5.0), 50.0) * 0.002;
+   //float t = 0;
+   //float s = 0.2;
+   FragColor = vec4(
+      color.x * clock + s + t,
+      color.y * clock + s + t,
+      color.z * clock + s + t,
+      1.0
+   );
 }
